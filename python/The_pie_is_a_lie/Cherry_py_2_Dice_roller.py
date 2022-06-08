5123eeb7394b03be27b6f2f2730a236d92c22b20
Shroudoffrost <coder.av@yandex.ru>
2022-06-09 01:50:50 +0300

    IMTO_python_labs
    1,2,4 labs, including dice roller

---
+++ python/The_pie_is_a_lie/Cherry_py_2_Dice_roller.py
@@ -0,0 +1,41 @@
+from random import randint as dice
+import roll_round
+import time
+
+player_1 = input('Player One, introduce yourself: ')
+player_2 = input('Player Two, introduce yourself: ')
+total_for_player_1 = 0
+total_for_player_2 = 0
+
+print('GREATER DICE will choose how many times you are to roll\n')
+time.sleep(5)
+
+this_many = dice(1,10)
+if this_many == 1:
+    print('You will roll only once\n'
+        +'The winner is the one who gets more points\n'
+        +'START!\n'
+        +'Roll the dice, ', player_1)
+else:
+    print('You will roll '+str(this_many)+' times\n'
+        +'The winner is the one who gets more points after '+str(this_many)+' rolls\n'
+        +'START!\n'
+        +'Roll the dice, ', player_1)
+time.sleep(5)
+
+total_for_player_1 = roll_round.roll_round(dice, this_many)
+print(player_1,', you get '+str(total_for_player_1))
+time.sleep(3)
+print('Now it is '+player_2+'\'s turn to roll!\n START!')
+total_for_player_2 = roll_round.roll_round(dice, this_many)
+print(player_2,', you get '+str(total_for_player_2))
+
+time.sleep(5)
+print('We are done. Now let\'s see who won')
+time.sleep(5)
+if total_for_player_1 > total_for_player_2:
+    print(player_1+' is the winner!')
+elif total_for_player_1 < total_for_player_2:
+    print(player_2+' is the winner!')
+else:
+    print('Draw, no winner')
