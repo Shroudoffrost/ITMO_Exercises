1ff1c083fa5b9ba1c31064312bcb7c6e1bec6fe7
Shroudoffrost <coder.av@yandex.ru>
2022-06-10 19:19:35 +0300

    Dice roller: minor code optimizations

--- python/The_pie_is_a_lie/Cherry_py_2_Dice_roller.py
+++ python/The_pie_is_a_lie/Cherry_py_2_Dice_roller.py
@@ -12,21 +12,21 @@ if this_many == 1:
     print('You will roll only once\n'
         +'The winner is the one who gets more points\n'
         +'START!\n'
-        +'Roll the dice, ', player_1)
+        +'Roll the dice, '+player_1)
 else:
     print('You will roll '+str(this_many)+' times\n'
         +'The winner is the one who gets more points after '+str(this_many)+' rolls\n'
         +'START!\n'
-        +'Roll the dice, ', player_1)
+        +'Roll the dice, '+player_1)
 time.sleep(5)
 
 total_for_player_1 = roll_round.roll_round(dice, this_many)
-print(player_1,', you get '+str(total_for_player_1))
+print(player_1+', you get '+str(total_for_player_1))
 time.sleep(3)
 
 print('Now it is '+player_2+'\'s turn to roll!\n START!')
 total_for_player_2 = roll_round.roll_round(dice, this_many)
-print(player_2,', you get '+str(total_for_player_2))
+print(player_2+', you get '+str(total_for_player_2))
 
 time.sleep(2)
 print('We are done. Now let\'s see who won')
