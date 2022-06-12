8930b3b2ef8f451a99bcc0d83d592ee3a13ef41f
Shroudoffrost <coder.av@yandex.ru>
2022-06-12 22:52:48 +0300

    Algorithm encoded: bug fixed and minor code optimizations

--- python/The_pie_is_a_lie/Cherry_pie_3/Algorithm_encoded.py
+++ python/The_pie_is_a_lie/Cherry_pie_3/Algorithm_encoded.py
@@ -1,5 +1,4 @@
 import playsound
-from time import sleep 
 
 n = 0
 s = 0
@@ -7,20 +6,17 @@ i = 1
 
 welcome = 'Cherry_pie_3\Assets\Messages\Welcome.mp3'
 buzzer = 'Cherry_pie_3\Assets\Messages\Buzzer.mp3'
-playsound(welcome)
+playsound.playsound(welcome)
 
 while n <= 0:
     playsound.playsound(buzzer)
     n = int(input())
 
-for i in range(1,n):
-    playsound(buzzer)
+for i in range(1,n+1):
+    playsound.playsound(buzzer)
     d = int(input())
     s += d
     i = i + 1 
 
-if n == 1:
-    mid = s/i
-else:    
-    mid = s/(i-1)
+mid = s/(i-1)
 print(mid)
