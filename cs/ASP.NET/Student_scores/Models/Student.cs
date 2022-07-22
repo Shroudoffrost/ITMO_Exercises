d93c7feaca1e42ad349942b86108ae334166e8a0
Shroudoffrost <coder.av@yandex.ru>
2022-07-22 17:01:32 +0300

    Added Students_scores app, that saves student scores to db

---
+++ cs/ASP.NET/Student_scores/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace StudentScore.Models
+{
+    public class Student
+    {
+        public virtual int StudentId    { get; set; }
+        [DisplayName("FirstName")]
+        public virtual string FirstName { get; set; }
+        [DisplayName("LastName")]
+        public virtual string LastName  { get; set; }
+        [DisplayName("Group")]        
+        public virtual string Group     { get; set; }
+        [DisplayName("Task2")]
+        public virtual int ScoreTask1   { get; set; }
+        [DisplayName("Task2")]
+        public virtual int ScoreTask2   { get; set; }
+    }
+}
---
+++ cs/ASP.NET/Student_scores/Models/StudentDbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using StudentScore.Models;
+
+namespace Student_scores.Models
+{
+    public class StudentDbInitializer : DropCreateDatabaseIfModelChanges<StudentContext>
+    {
+        base.Seed(context);
+
+    }
+}
